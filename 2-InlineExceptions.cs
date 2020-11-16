4145ce1ce37b25c95358c226cd4d55be1259cb55
Jogger <beltchikov@gmx.de>
2020-11-16 14:29:28 +0100

    Inline exceptions

---
+++ 2-InlineExceptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp7
+{
+    public class InlineExceptions
+    {
+        public static void Demo()
+        {
+            object someValue = null;
+            var anotherValue = someValue ?? throw new ArgumentNullException();
+
+            var secondValue = someValue == null ? throw new InvalidOperationException() : someValue;
+        }
+    }
+}
